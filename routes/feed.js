7dd99c8fa3505fe4012d281b0034878900a37e87
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-20 21:17:20 +0600

    is auth middleware added to route

--- routes/feed.js
+++ routes/feed.js
@@ -8,8 +8,8 @@ const isAuth = require('../middleware/is-auth');
 const { multerHandler } = require('../utils/multer');
 
 router.get('/posts', isAuth, feedController.getPosts);
-router.get('/post/:postId', feedController.getPostByID);
-router.post('/post', validator.postValidator, multerHandler, feedController.createPost);
-router.put('/post/:postId', feedController.updatePost);
-router.delete('/post/:postId', feedController.deletePost);
+router.get('/post/:postId', isAuth, feedController.getPostByID);
+router.post('/post', isAuth, validator.postValidator, multerHandler, feedController.createPost);
+router.put('/post/:postId', isAuth, feedController.updatePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 module.exports = router;
