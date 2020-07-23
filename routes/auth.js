dad58d219d931242f8dc0e03c3e1ab74d2da1fe0
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-23 20:03:17 +0600

    middleware add on route

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const authController = require('../controllers/auth');
 const validator = require('../middleware/validator');
-
+const isAuth = require('../middleware/is-auth');
 router.post('/signup', validator.userValidator, authController.signup);
 router.post('/login', authController.login);
-router.get('/status', authController.getUserStatus)
+router.get('/status', isAuth, authController.getUserStatus)
 module.exports = router;
