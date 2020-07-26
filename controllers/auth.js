ae9114224df3cfcb3cd68c8b1cc6f3eb81b77508
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-26 21:39:18 +0600

    update getUserStatus

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,23 +82,22 @@ exports.getUserStatus = (req, res, next) => {
 
 exports.updateUserStatus = (req, res, next) => {
     const newStatus = req.body.status;
-    user.findById(req.userId)
-        .then(user => {
-            if (!user) {
-                const error = new Error('User not found');
-                error.statusCode = 404;
-                throw error;
-            }
-            user.status = newStatus;
-            return user.save();
-        })
-        .then(result => {
-            res.status(200).json({ message: 'User Updated' })
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500
-            }
-            next(err);
-        })
+    try {
+        const user = await User.findById(req.userId)
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        user.status = newStatus;
+        await user.save();
+
+
+        res.status(200).json({ message: 'User Updated' })
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err);
+    }
 }
